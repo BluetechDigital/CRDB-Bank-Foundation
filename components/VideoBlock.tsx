ee7cb613e9f33cb0cbdfb8208f3174b18ad0ea6e
Todd Mpeli <toddowenmpeli@rocketmail.com>
2024-04-14 15:14:55 +0100

    UPdated styling.

--- components/VideoBlock.tsx
+++ components/VideoBlock.tsx
@@ -40,7 +40,7 @@ const VideoBlock: FC<IVideoBlock> = ({
 					" py-12 px-4 bg-white bg-cover bg-no-repeat bg-center"
 				}
 			>
-				<div className="container mx-auto flex flex-col lg:flex-row items-center justify-center gap-4 lg:gap-x-4 xl:gap-x-16">
+				<div className="container mx-auto flex flex-col xl:flex-row items-center justify-center gap-4 lg:gap-16">
 					<motion.div
 						initial={initial}
 						variants={stagger}
@@ -48,7 +48,7 @@ const VideoBlock: FC<IVideoBlock> = ({
 						viewport={{once: true}}
 						className={
 							title && highlightText && paragraph
-								? "max-w-2xl mx-auto text-center lg:max-w-5xl w-full lg:w-[50%] xl:w-[35%]"
+								? "max-w-2xl mx-auto text-center lg:max-w-5xl w-full xl:w-[35%]"
 								: "hidden"
 						}
 					>
@@ -57,7 +57,7 @@ const VideoBlock: FC<IVideoBlock> = ({
 								initial={initialTwo}
 								whileInView={fadeIn}
 								viewport={{once: true}}
-								className="text-center lg:text-left text-paragraph text-green-two"
+								className="text-center xl:text-left text-paragraph text-green-two mb-5"
 							>
 								{subtitle}
 							</motion.h4>
@@ -66,7 +66,7 @@ const VideoBlock: FC<IVideoBlock> = ({
 								variants={stagger}
 								whileInView="animate"
 								viewport={{once: true}}
-								className="leading-[3.5rem] my-2 max-w-2xl mx-auto mb-6 text-center lg:text-left font-semibold text-4xl lg:text-5xl"
+								className="leading-[3.5rem] my-2 max-w-2xl mx-auto mb-6 text-center xl:text-left font-semibold text-4xl lg:text-5xl"
 							>
 								{title}
 								<span className="p-2 ml-3 bg-green-two text-white">
@@ -76,7 +76,7 @@ const VideoBlock: FC<IVideoBlock> = ({
 						</div>
 						<Paragraph
 							content={paragraph}
-							tailwindStyling="text-center lg:text-left text-black text-paragraph"
+							tailwindStyling="max-w-2xl mx-auto xl:mx-0 text-center xl:text-left text-black text-paragraph"
 						/>
 						<Link
 							href={`${buttonLink?.url}`}
@@ -84,7 +84,7 @@ const VideoBlock: FC<IVideoBlock> = ({
 							aria-label={`${buttonLink?.title}`}
 							className={
 								buttonLink?.url
-									? "flex mt-6 lg:mt-0 items-center justify-center lg:justify-start"
+									? "flex mt-6 xl:mt-0 items-center justify-center xl:justify-start"
 									: "hidden"
 							}
 						>
@@ -101,7 +101,7 @@ const VideoBlock: FC<IVideoBlock> = ({
 						whileInView={slideInRightFinish}
 						className={`${
 							displayVideo ? "h-fit" : "h-[300px] lg:h-[500px]"
-						} bg-center bg-no-repeat bg-cover mt-6 lg:mt-0 w-full lg:w-[50%] xl:w-[65%]`}
+						} bg-center bg-no-repeat bg-cover mt-6 lg:mt-0 w-full xl:w-[65%]`}
 						style={{
 							backgroundImage: `url("${
 								displayVideo ? "none" : videoBackgroundImage
