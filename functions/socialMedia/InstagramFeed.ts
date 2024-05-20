e11ed4d389a4ec8bf668e3dfaf937d65d1976c84
Todd Mpeli <toddowenmpeli@rocketmail.com>
2024-05-20 15:09:41 +0100

    Testing

--- functions/socialMedia/InstagramFeed.ts
+++ functions/socialMedia/InstagramFeed.ts
@@ -2,6 +2,5 @@ import {getInstagramFeed} from "../../pages/api/instagram-feed";
 
 export const getAllInstagramFeedContent = async () => {
 	const instagramFeed = await getInstagramFeed();
-	// Use the instagramFeed data here
 	return instagramFeed;
 };
